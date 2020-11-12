28110a040dd8bb4586922e5fb0d5546b8e555bba
Yegor Gangalo <gangaloyegor@gmail.com>
2020-11-12 13:19:03 +0200

    merge all js in one file script.js

---
+++ js/script.js
@@ -0,0 +1,36 @@
+(() => {
+  //HEADER-hight
+  const { height: pageHeaderHeight } = document
+        .querySelector(".header")
+        .getBoundingClientRect();
+  document.body.style.paddingTop = `${pageHeaderHeight}px`;
+
+  //MENU
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+
+  //MODAL
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
