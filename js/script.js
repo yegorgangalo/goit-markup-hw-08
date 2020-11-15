4166879a5356cec9572c550ce70ee4d49b0a48d2
Yegor Gangalo <gangaloyegor@gmail.com>
2020-11-16 01:51:31 +0200

    added js code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 (() => {
-  //HEADER-hight
+
+/* ---------HEADER-hight------------ */
   const { height: pageHeaderHeight } = document
         .querySelector(".header")
         .getBoundingClientRect();
@@ -18,21 +19,114 @@
 
     mobileMenuRef.classList.toggle("is-open");
   });
+/* =========================================== */
 
-  //MODAL
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
+/* --------MODAL window open/close------------- */
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    backdrop: document.querySelector("[data-backdrop]"),
+  };
 
-    if (refs.openModalBtn) {
+  if (refs.openModalBtn) {
       refs.openModalBtn.addEventListener("click", toggleModal);
       refs.closeModalBtn.addEventListener("click", toggleModal);
-      }
+      refs.backdrop.addEventListener('click', onBackdropClick);
+  };
 
-    function toggleModal() {
-      document.body.classList.toggle("modal-open");
-      refs.modal.classList.toggle("is-hidden");
+  function onBackdropClick(event) {
+      if (event.target === event.currentTarget) {
+        toggleModal();
+      };
+  };
+
+  function toggleModal() {
+    refs.backdrop.classList.contains("is-hidden")
+      ? window.addEventListener('keydown', onPressEscape)
+      : window.removeEventListener('keydown', onPressEscape);
+    refs.backdrop.classList.toggle("is-hidden");
+  };
+
+  function onPressEscape(event) {
+    if (event.code === 'Escape') {
+      toggleModal();
+    };
+  };
+
+/* add focus on modal */
+  const modalRef = document.querySelector('[data-modal]');
+
+  refs.openModalBtn.addEventListener('click', modalFocus);
+
+  function modalFocus() {
+    modalRef.focus();
+    modalRef.style.outline = 'none';
+  }
+
+/* CHECKBOX-icon in modal add CHECKED */
+  const checkboxIconRef = document.querySelector('[data-checkbox-icon]');
+  const checkboxInputRef = document.querySelector('[data-checkbox-input]');
+
+  checkboxIconRef.addEventListener('click', byClickChecked);
+  checkboxIconRef.addEventListener('focus', addEventListenerAddChecked);
+
+  function addEventListenerAddChecked() {
+    window.addEventListener('keydown', onPressEnterAdd);
+  };
+
+  function onPressEnterAdd(event) {
+      if ( event.target.classList.contains('checkbox-icon') && event.code === 'Enter') {
+        checkboxInputRef.setAttribute('checked', '');
+        checkboxIconRef.classList.add('checked');
+        console.log(`checked added. onPressEnterRemove added`);
+        window.removeEventListener('keydown', onPressEnterAdd);
+        window.addEventListener('keydown', onPressEnterRemove);
+      }
+  };
+
+  function onPressEnterRemove(event) {
+    if (event.target.classList.contains('checkbox-icon') && event.code === 'Enter') {
+      checkboxInputRef.removeAttribute('checked');
+      checkboxIconRef.classList.remove('checked');
+      console.log(`checked removed. added onPressEnterAdd`);
+      window.removeEventListener('keydown', onPressEnterRemove);
+      addEventListenerAddChecked();
     }
+  };
+
+  function byClickChecked() {
+    if (checkboxInputRef.hasAttribute('checked')) {
+      checkboxInputRef.removeAttribute('checked');
+      checkboxIconRef.classList.remove('checked');
+      addEventListenerAddChecked();
+    } else {
+      checkboxInputRef.setAttribute('checked', '');
+      checkboxIconRef.classList.add('checked');
+      window.addEventListener('keydown', onPressEnterRemove);
+    }
+  };
+
+/* ========================================= */
+
+/* --------FORM in modal window------------- */
+  const formModalRef = document.querySelector('[data-form-modal]');
+
+  const takeFormData = event => {
+    event.preventDefault();//забороняє браузеру відправляти форму при натисканні кнопки
+    // console.dir(event.target.elements); //так можна отримати доступ до елементів форми
+    const formRef = event.target;// тут міститься посилання на форму
+    const formData = new FormData(formRef);//створюємо новий об'єкт
+    const submittedData = {};//об'єкт для збору даних з форми, який надішлеться на бекенд
+
+    formData.forEach((value, key) => {//цей об'єкт просто має ф-цію форіч і дані інпутів у вигляді value та key = name інпута
+      submittedData[key] = value;//записуємо дані в об'єкт
+    });
+    console.dir(submittedData);
+  };
+
+  if(formModalRef){
+    formModalRef.addEventListener('submit', takeFormData);
+  };
+/* --------------------- */
+
 })();
